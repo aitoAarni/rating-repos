9b5fe2caf63e9fe82eab5bfd1c4d72fab4aca268
aitoAarni <aarni.rechardt@gmail.com>
2024-10-10 11:56:26 +0300

    repo list is visible

---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,18 @@
+import { Text } from "react-native"
+
+const RepositoryItem = ({item}) => {
+    return (
+        <Text>
+            Full name: {item.fullName}
+            Description: {item.description}
+            Language: {item.language}
+            Stars: {item.stargazersCount}
+            Forks: {item.forksCount}
+            Reviews: {item.reviewCount}
+            Rating: {item.ratingAverage}
+
+        </Text>
+    )
+}
+
+export default RepositoryItem
