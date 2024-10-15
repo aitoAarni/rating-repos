236347dca532ad839b6eeb37f13f65ec7c9cb786
aitoAarni <aarni.rechardt@gmail.com>
2024-10-15 19:03:46 +0300

    routing added to main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,21 +1,25 @@
-import { StyleSheet, View } from 'react-native'
-import RepositoryList from './RepositoryList'
-import AppBar from './AppBar'
+import { StyleSheet, View } from 'react-native';
+import RepositoryList from './RepositoryList';
+import AppBar from './AppBar';
+import { Navigate, Route, Routes } from 'react-router-native';
 
 const styles = StyleSheet.create({
-    container: {
-        flexGrow: 1,
-        flexShrink: 1,
-    },
-})
+  container: {
+    flexGrow: 1,
+    flexShrink: 1,
+  },
+});
 
 const Main = () => {
-    return (
-        <View style={styles.container}>
-            <AppBar />
-            <RepositoryList />
-        </View>
-    )
-}
+  return (
+    <View style={styles.container}>
+      <AppBar />
+      <Routes>
+        <Route path='/' element={<RepositoryList />} />
+        <Route path='*' element={<Navigate to="/" replace />} />
+      </Routes>
+    </View>
+  );
+};
 
-export default Main
+export default Main;
