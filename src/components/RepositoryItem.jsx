403c79294d44a423896eb90ebde48f1bce75b046
aitoAarni <aarni.rechardt@gmail.com>
2024-10-15 16:07:40 +0300

    syling front page repo layout

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -6,27 +6,94 @@ const styles = StyleSheet.create({
     container: {
         backgroundColor: theme.colors.container,
     },
+    topContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+    },
+    bottomContainer: {},
+    bottomContainerNums: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        flexGrow: 1,
+    },
+    bottomContainerTitles: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        flexGrow: 1,
+    },
+    bottomContainerChildren: {
+        flexGrow: 1,
+    },
     avatar: {
         width: 80,
         height: 80,
     },
     header: {},
+    description: {},
+    language: {},
 })
 
 const TopOfItem = ({ item }) => {
     return (
-        <View>
+        <View style={styles.topContainer}>
             <Image
                 style={styles.avatar}
                 source={{ uri: item.ownerAvatarUrl }}
             />
-            <Text
-                fontWeight={'bold'}
-                fontSize={'subheading'}
-                style={styles.header}
-            >
-                {item.fullName}
-            </Text>
+            <View>
+                <Text
+                    fontWeight={'bold'}
+                    fontSize={'subheading'}
+                    style={styles.header}
+                >
+                    {item.fullName}
+                </Text>
+                <Text style={styles.description}>{item.description}</Text>
+                <Text style={styles.language}>{item.language}</Text>
+            </View>
+        </View>
+    )
+}
+const BottomOfItem = ({ item }) => {
+    return (
+        <View style={styles.bottomContainer}>
+            <View style={styles.bottomContainerNums}>
+                <View style={styles.bottomContainerChildren}>
+                    <Text fontWeight={'bold'}>
+                        {item.stargazersCount < 1000
+                            ? item.stargazersCount.toString()
+                            : (item.stargazersCount / 1000).toFixed(1) + 'k'}
+                    </Text>
+                </View>
+                <View style={styles.bottomContainerChildren}>
+                    <Text fontWeight={'bold'}>
+                        {item.forksCount < 1000
+                            ? item.forksCountj.toString()
+                            : (item.forksCount / 1000).toFixed(1) + 'k'}{' '}
+                    </Text>
+                </View>
+                <View style={styles.bottomContainerChildren}>
+                    <Text fontWeight={'bold'}>{item.reviewCount}</Text>
+                </View>
+                <View style={styles.bottomContainerChildren}>
+                    <Text fontWeight={'bold'}>{item.ratingAverage}</Text>
+                </View>
+            </View>
+            <View style={styles.bottomContainerTitles}>
+                <View style={styles.bottomContainerChildren}>
+                    <Text>Stars</Text>
+                </View>
+                <View style={styles.bottomContainerChildren}>
+                    <Text>Forks</Text>
+                </View>
+                <View style={styles.bottomContainerChildren}>
+                    <Text>Reviews</Text>
+                </View>
+                <View style={styles.bottomContainerChildren}>
+                    <Text>Rating</Text>
+                </View>
+            </View>
         </View>
     )
 }
@@ -35,22 +102,7 @@ const RepositoryItem = ({ item }) => {
     return (
         <View>
             <TopOfItem item={item} />
-            <Text>
-                Full name: {item.fullName}
-                {'\n'}
-                Description: {item.description}
-                {'\n'}
-                Language: {item.language}
-                {'\n'}
-                Stars: {item.stargazersCount}
-                {'\n'}
-                Forks: {item.forksCount}
-                {'\n'}
-                Reviews: {item.reviewCount}
-                {'\n'}
-                Rating: {item.ratingAverage}
-                {'\n'}
-            </Text>
+            <BottomOfItem item={item} />
         </View>
     )
 }
