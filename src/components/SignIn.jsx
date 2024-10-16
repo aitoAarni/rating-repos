bb424abec54b99824cbdf769de15be93604f12bf
aitoAarni <aarni.rechardt@gmail.com>
2024-10-16 14:00:23 +0300

    sign in page form added, expo not working

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,7 +1,41 @@
-import Text from './Text';
+import { useFormik } from 'formik'
+import Text from './Text'
+import { View } from 'react-native-web'
+import { Pressable, TextInput } from 'react-native'
+
+const initialValues = {
+    username: '',
+    password: '',
+}
+
+const SignInForm = ({ onSubmit }) => {
+    const formik = useFormik({ initialValues, onSubmit })
+    return (
+        <View>
+            <TextInput
+                placeholder="username"
+                value={formik.values.username}
+                onChangeText={formik.handleChange}
+            />
+            <TextInput
+                secureTextEntry
+                placeholder="password"
+                value={formik.values.password}
+                onChange={formik.handleChange}
+            />
+            <Pressable onPress={formik.handleSubmit}>
+                <Text>Submit</Text>
+            </Pressable>
+        </View>
+    )
+}
 
 const SignIn = () => {
-  return <Text>The sign-in view</Text>;
-};
+    return <SignInForm onSubmit={onSubmit} />
+}
 
-export default SignIn;
+const onSubmit = values => {
+    console.log(values)
+}
+
+export default SignIn
