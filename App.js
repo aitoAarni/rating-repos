25daab98e48a3dd9c6354d7ff45db46b2aa2aaaf
aitoAarni <aarni.rechardt@gmail.com>
2024-10-20 21:39:49 +0300

    sing in token saved to local memory

--- App.js
+++ App.js
@@ -2,17 +2,19 @@ import { NativeRouter } from 'react-router-native'
 import { ApolloProvider } from '@apollo/client'
 import Main from './src/components/Main'
 import createApolloClient from './src/utils/apolloClient'
+import AuthStorageContext from './src/contexts/AuthStorageContex'
 import AuthStorage from './src/utils/authStorage'
 
-
-const authStorage = new AuthStorage('repoRate')
+const authStorage = new AuthStorage()
 const apolloClient = createApolloClient(authStorage)
 
 const App = () => {
     return (
         <NativeRouter>
             <ApolloProvider client={apolloClient}>
-                <Main />
+                <AuthStorageContext.Provider value={authStorage}>
+                    <Main />
+                </AuthStorageContext.Provider>
             </ApolloProvider>
         </NativeRouter>
     )
--- src/hooks/useSignIn.js
+++ src/hooks/useSignIn.js
@@ -1,12 +1,18 @@
-import { useMutation } from '@apollo/client'
+import { ApolloClient, useMutation } from '@apollo/client'
 import SIGN_IN from '../graphql/mutations'
+import useAuthStorage from '../contexts/useAuthStorage'
 
 const useSignIn = () => {
+    const authStorage = useAuthStorage()
     const [mutate, result] = useMutation(SIGN_IN)
 
     const signIn = async ({ username, password }) => {
-        console.log(`signIn func username: ${username}, password: ${password}`)
-        return mutate({ variables: { credentials: { username, password } } })
+        const { data } = await mutate({
+            variables: { credentials: { username, password } },
+        })
+        await authStorage.setAccessToken(data.authenticate.accessToken)
+        ApolloClient.resetClient()
+        return data
     }
 
     return [signIn, result]
