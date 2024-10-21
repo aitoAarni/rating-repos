17ec1d180fddca39f5fbf4766c65e8a76c0487ad
aitoAarni <aarni.rechardt@gmail.com>
2024-10-22 01:15:45 +0300

    ReopsitoryList filds show tests added

--- __tests__/components/RepositoryList.test.jsx
+++ __tests__/components/RepositoryList.test.jsx
@@ -35,14 +35,52 @@ const mocks = [
 ]
 
 describe('RepositoryList displays texts for repositories', () => {
-    it("displays repo's name", async () => {
+    beforeEach(() => {
         render(
             <MockedProvider mocks={mocks} addTypename={false}>
                 <RepositoryList />
             </MockedProvider>
         )
+    })
+    it("displays repo's name", async () => {
         await waitFor(() =>
             expect(screen.getByText('jaredpalmer/formik')).toBeDefined()
         )
+    })
+
+    it("displays repo's description", async () => {
+        await waitFor(() => {
+            expect(
+                screen.getByText('Build forms in React, without the tears')
+            ).toBeDefined()
+        })
+    })
+
+    it("displays repo's language", async () => {
+        await waitFor(() => {
+            expect(screen.getByText('TypeScript')).toBeDefined()
+        })
+    })
+    it("displays repo's forks", async () => {
+        await waitFor(() => {
+            expect(screen.getByText('2.8k')).toBeDefined()
+        })
+    })
+
+    it("displays repo's startGazerCount", async () => {
+        await waitFor(() => {
+            screen.debug()
+            expect(screen.getByText('33.9k')).toBeDefined()
+        })
+    })
+    it("displays repo's rating average", async () => {
+        await waitFor(() => {
+            expect(screen.getByText('90')).toBeDefined()
+        })
+    })
+    it("displays repo's review count", async () => {
+        await waitFor(() => {
+            expect(screen.getByText('5')).toBeDefined()
+        })
     })
 })
