4cdd480ea0192f39f2993c5c4f8a6eb01e081e7c
aitoAarni <aarni.rechardt@gmail.com>
2024-10-24 01:23:40 +0300

    reviewvs added to single rating page

--- src/components/SingleRepositoryItem.jsx
+++ src/components/SingleRepositoryItem.jsx
@@ -3,23 +3,82 @@ import useRepository from '../hooks/useRepository'
 import RepositoryItem from './RepositoryItem'
 import Text from './Text'
 import useComments from '../hooks/useComments'
+import { FlatList, StyleSheet, View } from 'react-native'
+import theme from '../theme'
+import formatDate from '../utils/formatDate'
+
+const styles = StyleSheet.create({
+    separator: {
+        height: 10,
+        backgroundColor: theme.colors.separator,
+    },
+    commentContainer: {
+        flexDirection: 'row',
+        margin: 15,
+    },
+    commentRightContainer: { flexShrink: 1 },
+    commentLeftContainer: { marginRight: 15 },
+
+    ratingCircle: {
+        height: 60,
+        width: 60,
+        borderRadius: 30,
+        borderWidth: 3,
+        borderColor: theme.colors.primary,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    commentRating: { color: theme.colors.primary, fontSize: 25 },
+    commentName: {},
+    commentDate: {},
+    commentText: {fontSize: 15},
+})
+
+const Separator = () => {
+    return <View style={styles.separator}></View>
+}
 
 const SingleRepositoryItem = () => {
     const { id } = useParams()
     const { repository, loading: loadingRepository } = useRepository(id)
     const { commentEdges, loading: loadingComments } = useComments(id)
-    if (loadingRepository) return <Text>loading...</Text>
     console.log(commentEdges)
     const comments = !loadingComments
         ? commentEdges.map(edge => edge.node)
         : null
     console.log(comments)
+    if (loadingRepository || loadingComments) return <Text>loading...</Text>
 
-    return <RepositoryItem item={repository} githubButton />
+    return (
+        <FlatList
+            ListHeaderComponent={() => (
+                <RepositoryItem item={repository} githubButton />
+            )}
+            data={comments}
+            renderItem={({ item }) => <Comment comment={item} />}
+            ItemSeparatorComponent={Separator}
+        />
+    )
 }
 
 const Comment = ({ comment }) => {
-    
+    return (
+        <View style={styles.commentContainer}>
+            <View style={styles.commentLeftContainer}>
+                <View style={styles.ratingCircle}>
+                    <Text fontWeight={'bold'} style={styles.commentRating}>
+                        {comment.rating}
+                    </Text>
+                </View>
+            </View>
+            <View style={styles.commentRightContainer}>
+                <Text fontWeight="bold">{comment.user.username}</Text>
+                <Text>{formatDate(comment.createdAt)}</Text>
+                <Text style={styles.commentText}>{comment.text}</Text>
+            </View>
+        </View>
+    )
 }
 
 export default SingleRepositoryItem
