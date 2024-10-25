b6a908177b9aa64ab4f7a515a526cba7dbeb7099
aitoAarni <aarni.rechardt@gmail.com>
2024-10-26 01:36:07 +0300

    repo order component visible, but not working

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,8 +1,10 @@
-import { FlatList, View, StyleSheet, Pressable } from 'react-native'
+import { FlatList, View, StyleSheet, Pressable, ViewBase } from 'react-native'
 import RepositoryItem from './RepositoryItem'
 import theme from '../theme'
 import useRepositories from '../hooks/useRepositories'
 import { useNavigate } from 'react-router-native'
+import { useState } from 'react'
+import { Picker } from '@react-native-picker/picker'
 
 const styles = StyleSheet.create({
     separator: {
@@ -15,6 +17,10 @@ const ItemSeparator = () => <View style={styles.separator} />
 
 const RepositoryList = () => {
     const { repositories } = useRepositories()
+    const [orderedBy, setOrderedBy] = useState({
+        orderDirection: 'ASC',
+        orderedBy: 'CREATED_AT',
+    })
     const navigate = useNavigate()
 
     // Get the nodes from the edges array
@@ -23,21 +29,50 @@ const RepositoryList = () => {
         : []
 
     return (
-        <FlatList
-            data={repositoryNodes}
-            ItemSeparatorComponent={ItemSeparator}
-            renderItem={({ item }) => {
-                return (
-                    <Pressable
-                        onPress={() => {
-                            navigate(`/repository/${item.id}`)
-                        }}
-                    >
-                        <RepositoryItem item={item} />
-                    </Pressable>
-                )
-            }}
-        />
+        <View>
+            <Picker
+                selectedValue={orderedBy}
+                // eslint-disable-next-line no-unused-vars
+                onValueChange={(itemValue, _itemIndex) => {
+                    console.log()
+                    setOrderedBy(itemValue)
+                }}
+            >
+                <Picker.Item
+                    label="Latest repositories"
+                    value={{ orderDirection: 'ASC', orderedBy: 'CREATED_AT' }}
+                />
+                <Picker.Item
+                    label="Highes rated repositories"
+                    value={{
+                        orderDirection: 'DESC',
+                        orderedBy: 'RATING_AVERAGE',
+                    }}
+                />
+                <Picker.Item
+                    label="Lowest rated repositories"
+                    value={{
+                        orderDirection: 'ASC',
+                        orderedBy: 'RATING_AVERAGE',
+                    }}
+                />
+            </Picker>
+            <FlatList
+                data={repositoryNodes}
+                ItemSeparatorComponent={ItemSeparator}
+                renderItem={({ item }) => {
+                    return (
+                        <Pressable
+                            onPress={() => {
+                                navigate(`/repository/${item.id}`)
+                            }}
+                        >
+                            <RepositoryItem item={item} />
+                        </Pressable>
+                    )
+                }}
+            />
+        </View>
     )
 }
 
